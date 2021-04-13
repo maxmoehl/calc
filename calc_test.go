b63985aa992f4c206602c0772949ad9caa8198b9
Maximilian Moehl <maximilian.moehl@sap.com>
2021-04-13 11:38:45 +0200

    add executable to .gitignore and fix tests

--- calc_test.go
+++ calc_test.go
@@ -44,7 +44,7 @@ func Test_run(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := run(tt.arg)
+			got, err := Eval(tt.arg)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("run() error = %v, wantErr %v", err, tt.wantErr)
 				return
