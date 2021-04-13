8ffe16624a571e99c55b3d1bcc22b39e1b6096bf
Maximilian Moehl <maximilian.moehl@sap.com>
2021-04-13 11:36:01 +0200

    seperate package contentes and cli application

---
+++ cli/calc.go
@@ -0,0 +1,25 @@
+// This application is the cli version of calc. It serves as a basic wrapper to
+// enable the use of calc as a package in an application.
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/maxmoehl/calc"
+
+	"github.com/fatih/color"
+)
+
+func main() {
+	res, err := calc.Eval(strings.Join(os.Args[1:], ""))
+	if _, found := os.LookupEnv("DEBUG"); found {
+		calc.SetDebug(true)
+	}
+	if err != nil {
+		color.Red(err.Error())
+		os.Exit(1)
+	}
+	fmt.Printf("%f\n", res)
+}
